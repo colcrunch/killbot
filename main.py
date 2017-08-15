f4f17319ba3079beff9e1bfb9cd99bc5f6a5dfdb
Robert Hartnett <rhartnett35@gmail.com>
2017-08-15 17:24:26 -0400

    Add time command.

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-import os
 import config
+
 import discord
-import datetime
 from discord.ext.commands import Bot
+
+import datetime
+
 
 import requests
 
 killbot = Bot(command_prefix=config.PREFIX)
 
 @killbot.event
-async def on_read():
+async def on_ready():
     print("Bot online")
 
 @killbot.command()
 async def  ping():
     return await killbot.say("Pong!")
 
-@killbot.command()
+@killbot.command(aliases = ['eve_time', 'evetime', 't'])
 async def time():
-    return await datetime.datetime.utcnow("HH:mm")
+    time = datetime.datetime.utcnow()
+    return await killbot.say("Current Eve (UTC) Time: " + time.strftime("%H:%M "))
 
 killbot.run(config.BOT_TOKEN)
