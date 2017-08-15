bf60a09daebf19596b44ff63e474f1e19fdb8f5b
Robert Hartnett <rhartnett35@gmail.com>
2017-08-15 16:24:59 -0400

    Add config and beginnings of bot.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
+import config
 import discord
+import datetime
+from discord.ext.commands import Bot
 
-print('Hello, World')
+import requests
+
+killbot = Bot(command_prefix=config.PREFIX)
+
+@killbot.event
+async def on_read():
+    print("Bot online")
+
+@killbot.command()
+async def  ping():
+    return await killbot.say("Pong!")
+
+@killbot.command()
+async def time():
+    return await datetime.datetime.utcnow("HH:mm")
+
+killbot.run(config.BOT_TOKEN)
